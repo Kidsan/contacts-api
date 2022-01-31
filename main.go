4659114ed5d4e877a3cc4c5f9748bfba26abbb4e
Kieran O'Sullivan <kieran.osullivan@rewe-digital.com>
2022-01-31 19:56:19 +0100

    add basic handlers

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
 
-type MyHandler struct{}
+	"github.com/go-chi/chi/v5"
+)
 
-func (m MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("received"))
+type ContactsHandler struct {
+	data []string
+}
+
+func (c *ContactsHandler) Get(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(strings.Join(c.data, ",")))
+}
+
+func (c *ContactsHandler) Post(w http.ResponseWriter, r *http.Request) {
+	c.data = append(c.data, c.data[0]+"a")
+	w.Write([]byte(c.data[len(c.data)-1]))
 }
 
 func main() {
-	handler := MyHandler{}
-	http.HandleFunc("/", handler.ServeHTTP)
-	http.ListenAndServe("0.0.0.0:3000", nil)
+	r := chi.NewRouter()
+	handler := ContactsHandler{}
+	handler.data = append(handler.data, "hello")
+	r.Get("/", handler.Get)
+	r.Post("/", handler.Post)
+	http.ListenAndServe(":3000", r)
 }
